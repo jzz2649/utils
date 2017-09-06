23a7f001a717faff0f8bda15e32a4b762d34002e
jzz2649 <1563382649@qq.com>
2017-09-07 01:00:59 +0800

    Update clone.js

--- clone.js
+++ clone.js
@@ -6,23 +6,6 @@ function isArray(arr) {
     return type(arr) === '[object Array]';
 }
 
-function isNumber(num) {
-    return type(num) === '[object Number]';
-}
-
-function isNaN(nan) {
-    if(isNumber(nan)){
-        if(!parseInt(nan)&&parseInt(nan)!==0){
-            return true;
-        }
-    }
-    return false;
-}
-
-function isFun(fun) {
-    return type(fun) === '[object Function]';
-}
-
 function isObject(obj) {
     return type(obj) === '[object Object]';
 }
