55bc9e6aa78a477d50ee3cf27a02c2e60ebfecc0
jzz2649 <1563382649@qq.com>
2017-09-07 00:36:00 +0800

    Update formatDate.js

--- formatDate.js
+++ formatDate.js
@@ -1,59 +1,48 @@
-function formatDate(timestamp,type){
-    var result,year,month,hours,minute,second;
-    var type=type||'YY-MM-DD hh:mm:ss';
+function formatDate(timestamp, type){
+    var year, month, hours, minute, second, d, t;
+    var type = type || 'YY-MM-DD hh:mm:ss';
     var date = new Date(timestamp||null);
     var len2 = function(v){
-        var v = v?v.toString():'';
+        var v = v ? v.toString() : '';
         var result = '00';
         var len = v.length;
-        if(len===1){
+        if (len === 1) {
             result = '0'+v;
-        }else if (len===2){
+        } else if (len === 2) {
             result = v;
-        }else if(len>2){
-            result = v.slice(0,2);
+        } else if (len > 2) {
+            result = v.slice(0, 2);
         }
         return result;
     }
-    var _formatDate = function(d,t){
+    var _formatDate = function(d, t){
         var date = '';
         var time = '';
-        if(d){
-            date = year+d+month+d+day;
+        if (d) {
+            date = year + d + month + d + day;
         }
-        if(t){
-            time = hours+t+minute+t+second;
+        if (t) {
+            time = hours + t + minute + t + second;
         }
         return (date+' '+time).trim();
     }
     year = date.getFullYear();
-    month = len2(date.getMonth()+1);
+    month = len2(date.getMonth() + 1);
     day = len2(date.getDate());
     hours = len2(date.getHours());
     minute = len2(date.getMinutes());
     second = len2(date.getSeconds());
-    switch(type){
-        case 'YY-MM-DD hh:mm:ss':
-            result = _formatDate('-',':');
-        break;
-        case 'YY/MM/DD hh:mm:ss':
-            result = _formatDate('/',':');
-        break;
-        case 'YY-MM-DD':
-            result = _formatDate('-');
-        break;
-        case 'YY/MM/DD':
-            result = _formatDate('/');
-        break;
-        case 'hh/mm/ss':
-            result = _formatDate(undefined,'/');
-        break;
-        case 'hh-mm-ss':
-            result = _formatDate(undefined,'-');
-        break;
-        case 'hh:mm:ss':
-            result = _formatDate(undefined,':');
-        break;
+    if (type.length === 17) {
+        d = type.slice(2, 3);
+        t = type.slice(11, 12);
+    } else if (type.length === 8) {
+        if (~type.indexOf('YY')) {
+            d = type.slice(2, 3);
+        } else {
+            t = type.slice(2, 3);
+        }
+    } else {
+        throw new Error('args type error,for example YY-MM-DD hh:mm:ss');
     }
-    return result;
+    return _formatDate(d, t);
 }
