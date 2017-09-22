ff4581d4abd59c1c4a837c80aa64e283bc3dedc3
jzz2649 <1563382649@qq.com>
2017-09-23 00:08:09 +0800

    Update type.js

--- type.js
+++ type.js
@@ -32,28 +32,28 @@ function isObject(obj) {
 }
 
 function strIsEmpty(str){
-	return str.trim() === '';
+    return str.trim() === '';
 }
 
 function objIsEmpty(obj){
-	for(var i in obj){
-		return false;
-	}
-	return true;
+    for(var i in obj){
+        return false;
+    }
+    return true;
 }
 
 function arrIsEmpty(arr){
-	for(var i = 0; i < arr.lenth; i++){
-		return false;
-	}
-	return true;
+    for(var i = 0; i < arr.lenth; i++){
+        return false;
+    }
+    return true;
 }
 
 function isEmpty(o){
-	if(o === undefined || o === null) return true;
-	if(isString(o) && strIsEmpty(o)) return true;
-	if(isObject(o) && objIsEmpty(o)) return true;
-	if(isArray(o) && arrIsEmpty(o)) return true;
-	if(isNaN(o)) return true;
-	return false;
+    if(o === undefined || o === null) return true;
+    if(isString(o) && strIsEmpty(o)) return true;
+    if(isObject(o) && objIsEmpty(o)) return true;
+    if(isArray(o) && arrIsEmpty(o)) return true;
+    if(isNaN(o)) return true;
+    return false;
 }
