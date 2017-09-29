6c938665d3d6a4e62e9948143b930bc16de770b7
jzz2649 <1563382649@qq.com>
2017-09-29 15:38:24 +0800

    Update filter.js

--- filter.js
+++ filter.js
@@ -9,19 +9,19 @@ import { isEmpty } from './empty.js';
  */
 
 function filterValue(o){
-    var d;
+    var d = o;
     if(isObject(o)){
         d = {};
         for(var i in o){
             if(!isEmpty(o[i])){
-                d[i] = o[i];
+                d[i] = filterValue(o[i]);
             }
         }
     }else if(isArray(o)){
         d = [];
         for(var i = 0; i < o.length; i++){
             if(!isEmpty(o[i])){
-                d.push(o[i]);
+                d.push(filterValue(o[i]));
             }
         }
     }
