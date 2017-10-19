b49c7952020851fea6fc55f7e8fec3575e4eda49
jzz2649 <1563382649@qq.com>
2017-10-19 11:39:23 +0800

    Update number.js

--- number.js
+++ number.js
@@ -1,4 +1,6 @@
 import { paramToArray } from './convert.js';
+import { isEmpty } from './empty.js';
+import { isString, isObject, isArray, isFun, isNumber } from './type.js';
 
 function add(){
     var numbers = paramToArray(arguments);
@@ -7,4 +9,21 @@ function add(){
         s += numbers[i];
     }
     return s;
+}
+
+function AnyToNumber(any) {
+    switch(true){
+        case isEmpty(any):
+        case isFun(any):
+        case isArray(any):
+        case isObject(any):
+            return 0;
+        break;
+        case isString(any):
+            return parseFloat(any);
+        break;
+        case isNumber(any):
+            return any;
+        break;
+    }
 }
