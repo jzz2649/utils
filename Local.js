557e699cb4ddbda6f362fea3be7d8af1823f9d08
jzz2649 <1563382649@qq.com>
2019-04-18 12:40:11 +0800

    优化

--- Local.js
+++ Local.js
@@ -1,49 +1,89 @@
 function Local(store){
-  if(store&&toString.call(store)!=='[object Object]'){
-    throw new Error('Not is object');
-  }
-  var local = {};
-
-  var has = function(o, prop){
-    return o.hasOwnProperty(prop);
-  }
-
-  var is = function(x, y){
-    if (x === y) {
-      return x !== 0 || 1 / x === 1 / y;
-    } else {
-      return x !== x && y !== y;
+  var local = Object.create(null);
+  var event = [];
+  var change = function(){
+    for(var i=0, l = event.length; i<l; i++){
+      event[i].call(storage, storage);
     }
-  };
-
+  }
+  var keys = function(obj, fn){
+    for(var k in obj){
+      if(hasOwnProperty.call(obj, k)){
+        fn(k);
+      }
+    }
+  }
+  var copy = function(obj){
+    var set = {};
+    keys(obj, function(k){
+      set[k] = obj[k];
+    })
+    return set;
+  }
   var storage = {
-    set: function(k, v){
-      if(has(local, k)&&is(local[k],v)){
+    length: 0,
+    set: function(k, v) {
+      if(typeof v !== 'string'){
+        v = String(v);
+      }
+      if(local[k] === v){
         return;
+      }
+      if(local[k] === undefined){
+        ++storage.length;
       }
       local[k] = v;
       localStorage.setItem(k, v);
+      change();
     },
-    get: function(k){
-      return k ? local[k]: local;
+    get: function(k) {
+      if(local[k] === undefined){
+        return null;
+      }
+      return local[k];
     },
-    clear: function(){
-      local = {};
+    getAll: function(){
+      return copy(local);
+    },
+    remove: function(k) {
+      if(local[k] === undefined){
+        return;
+      }
+      --storage.length;
+      delete local[k];
+      localStorage.removeItem(k);
+      change();
+    },
+    clear: function() {
+      if(storage.length === 0){
+        return;
+      }
+      storage.length = 0;
+      local = Object.create(null);
       localStorage.clear();
+      change();
+    },
+    on: function(fn){
+      var index = event.indexOf(fn);
+      if(index > -1){
+        return;
+      }
+      event.push(fn);
+    },
+    off: function(fn){
+      var index = event.indexOf(fn);
+      if(index > -1){
+        event.splice(index, 1);
+      }
     }
   }
-
-  for(var item in localStorage){
-    if(has(localStorage, item)){
-      local[item] = localStorage.getItem(item);
-    }
-  }
-
-  if (store) {
-    storage.clear();
-    for(var key in store){
-      storage.set(key, store[key]);
-    }
+  keys(localStorage, function(k){
+    storage.set(k, localStorage.getItem(k));
+  })
+  if(typeof store === 'object' && store !== null){
+    keys(store, function(k){
+      storage.set(k, store[k]);
+    })
   }
   return storage;
 }
